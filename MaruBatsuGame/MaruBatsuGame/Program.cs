d9fec47c243594d0f822ff918ea15d0e69451e62
DESKTOP-NL3RUT3\宮澤こずえ <k.miyazawa@vision-sys.co.jp>
2025-05-14 16:03:30 +0900

    難易度選択の条件を変更

--- MaruBatsuGame/MaruBatsuGame/Program.cs
+++ MaruBatsuGame/MaruBatsuGame/Program.cs
@@ -1,58 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Runtime.CompilerServices;
-
 public class Program
 {
+    //playerクラス　CPUクラス（強弱2パターン）を作成
+    //空欄の場所を選択するインターフェースを作成
+
     private static List<(int, int)> emptyCells = new List<(int, int)>();//空欄の情報を入れるリスト
     private static int cpuLevel = 0;//CPUの難易度を保存
 
     static void Main()
     {
         // CPUのレベル選択
-        bool isLevelInputSuccessful = false;
-        while (!isLevelInputSuccessful)
+        while (true)
         {
-            isLevelInputSuccessful = InputLevel();
-            if (!isLevelInputSuccessful)
+            if (InputLevel())
+            {
+                break;
+            }
+            else
             {
                 Console.WriteLine("無効な入力です。もう一度入力してください。");
             }
         }
 
         //〇×ゲームのボード作成
-        string[,] input = new string[,]
+
+        //盤面の状態を空白付きで保持しているが、0,1,2などで持った方がよい→0:初期値,1:1Pの置き場所,2:2Pの置き場所
+        int[,] state = new int[,]
         {
-            {"   ","   ","   "},
-            {"   ","   ","   "},
-            {"   ","   ","   "}
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0}
         };
 
         //ボードの初期状態の表示
-        WriteBoard(input);
+        WriteBoard(state);
 
-        //難易度普通
         //int turn = 0;
-        //入力回数が９未満で"　"が０個ではない時ゲーム終了
-        while (/*turn < 9*/ !IsBoardFull(input))
+        //入力が1Pと2Pが置いたことで1ループとなっているので、概念が少し歪になっている。１コマで１ループがよい
+        while (/*turn < 9*/ !IsBoardFull(state))
         {
-            //ユーザー側の入力
-            bool isPlayerTurnSuccessful = InputNumber(input);
+            //プレイヤー側の入力
+            bool isPlayerTurnSuccessful = InputNumber(state);
 
             //勝敗チェック
-            if (CheckWinner(input, " ○ "))
+            if (CheckWinner(state, 1))
             {
                 Console.Clear();
 
-                WriteBoard(input);
+                WriteBoard(state);
 
-                GameOver(" ○ ");
+                GameOver(1);
 
                 return;
             }
             else
             {
-                WriteBoard(input);
+                WriteBoard(state);
             }
 
             //入力成功時　CPUのターン
@@ -61,22 +65,22 @@ public class Program
                 Console.Clear();
 
                 //CPU側の入力
-                ChoiceCpuNumber(input);
+                ChoiceCpuNumber(state);
 
                 //勝敗チェック
-                if (CheckWinner(input, " × "))
+                if (CheckWinner(state, 2))
                 {
                     Console.Clear();
 
-                    WriteBoard(input);
+                    WriteBoard(state);
 
-                    GameOver(" × ");
+                    GameOver(2);
 
                     return;
                 }
                 else
                 {
-                    WriteBoard(input);
+                    WriteBoard(state);
                 }
             }
         }
@@ -107,22 +111,22 @@ public class Program
         }
     }
 
-    private static void WriteBoard(string[,] input)//〇×ゲームのボード
+    private static void WriteBoard(int[,] state)//〇×ゲームのボード
     {
         Console.WriteLine("+---+---+---+");
-        Console.WriteLine($"|{input[0, 0]}|{input[0, 1]}|{input[0, 2]}|");
+        Console.WriteLine($"|{state[0, 0]}|{state[0, 1]}|{state[0, 2]}|");
         Console.WriteLine("+---+---+---+");
-        Console.WriteLine($"|{input[1, 0]}|{input[1, 1]}|{input[1, 2]}|");
+        Console.WriteLine($"|{state[1, 0]}|{state[1, 1]}|{state[1, 2]}|");
         Console.WriteLine("+---+---+---+");
-        Console.WriteLine($"|{input[2, 0]}|{input[2, 1]}|{input[2, 2]}|");
+        Console.WriteLine($"|{state[2, 0]}|{state[2, 1]}|{state[2, 2]}|");
         Console.WriteLine("+---+---+---+");
     }
 
-    private static bool InputNumber(string[,] input)//ユーザー側の入力
+    private static bool InputNumber(int[,] state)//プレイヤー側の入力
     {
         Console.WriteLine("どこに置きますか？（１～９）");
 
-        MakeEmptycellsList(input);
+        MakeEmptycellsList(state);
 
         if (emptyCells.Count > 0)
         {
@@ -137,10 +141,10 @@ public class Program
                     int row = (num - 1) / 3;
                     int col = (num - 1) % 3;
 
-                    //空欄の確認　"　"なら〇が入力できる
-                    if (input[row, col] == "   ")
+                    //空欄の確認　0なら入力できる
+                    if (state[row, col] == 0)
                     {
-                        input[row, col] = " ○ ";
+                        state[row, col] = 1;
                         return true;
                     }
                     else
@@ -168,73 +172,74 @@ public class Program
         }
     }
 
-    private static void ChoiceCpuNumber(string[,] input)//CPU側の入力
+    private static void ChoiceCpuNumber(int[,] state)//CPU側の入力
+                                                     //→優先順位　５のマスをとる＞４隅をとる＞相手の勝ちを阻止する>ダブルリーチの状態を作る
     {
-        MakeEmptycellsList(input);
-        List<(int, int)> cornerList = new List<(int, int)> { (0, 0), (0, 2), (2, 0), (2, 2) };
-
+        MakeEmptycellsList(state);
+        var cornerList = new List<(int, int)> { (0, 0), (0, 2), (2, 0), (2, 2) };
 
         if (emptyCells.Count > 0)
         {
-            if (cpuLevel == 1)
+            if (cpuLevel == 1)//空いてる個所にランダム入力
             {
                 Random n = new Random();
                 var (row, col) = emptyCells[n.Next(emptyCells.Count)];
-                input[row, col] = " × ";
+                state[row, col] = 2;
             }
 
-            if (cpuLevel == 2)
+            if (cpuLevel == 2)//勝つために優先順位に応じて入力
             {
-                if (input[1, 1] == "   ")
+                if (state[1, 1] == 0)//５のマス優先的に入力
                 {
-                    input[1, 1] = " × ";
+                    state[1, 1] = 2;
                 }
-                else if(cornerList.Count > 0 && input[row, col] == "   ")
+                else if (cornerList.Count > 0)//４隅を優先的に入力
                 {
                     Random n = new Random();
                     var (row, col) = cornerList[n.Next(cornerList.Count)];
-                    input[row, col] = " × ";
+                    state[row, col] = 2;
                 }
                 else
                 {
                     Random n = new Random();
                     var (row, col) = emptyCells[n.Next(emptyCells.Count)];
-                    input[row, col] = " × ";
+                    state[row, col] = 2;
 
                 }
             }
         }
     }
 
-    private static void MakeEmptycellsList(string[,] input)//空欄を確認し、格納するリストを作成
+    private static void MakeEmptycellsList(int[,] state)//空欄を確認し、格納するリストを作成
     {
         //リストのリセット
         emptyCells.Clear();
 
-        for (int i = 0; i < input.GetLength(0); i++)
+        for (int i = 0; i < state.GetLength(0); i++)
         {
-            for (int j = 0; j < input.GetLength(1); j++)
+            for (int j = 0; j < state.GetLength(1); j++)
             {
-                if (input[i, j] == "   ")
+                if (state[i, j] == 0)
                 {
-                    emptyCells.Add((i, j));//"　"の数をカウントしてリストに加える
+                    emptyCells.Add((i, j));//0の数をカウントしてリストに加える
                 }
             }
         }
     }
 
-    private static bool IsBoardFull(string[,] input)//"　"が０個ならtrueを返す
+    private static bool IsBoardFull(int[,] state)//0が０個ならtrueを返す
     {
-        MakeEmptycellsList(input);
+        MakeEmptycellsList(state);
         return emptyCells.Count == 0;
     }
 
-    private static bool CheckWinner(string[,] input, string target)//縦横対角線の入力チェック
+    private static bool CheckWinner(int[,] state, int target)//縦横対角線の入力チェック
+                                                             //→勝ちパターンのチェックに変更【123,456,789,147,258,369,159,257】の８パターン
     {
-        //横列のチェック
-        for (int i = 0; i < 3; i++)
+            //横列のチェック
+            for (int i = 0; i < 3; i++)
         {
-            if (input[i, 0] == target && input[i, 1] == target && input[i, 2] == target)
+            if (state[i, 0] == target && state[i, 1] == target && state[i, 2] == target)
             {
                 return true;
             }
@@ -243,15 +248,15 @@ public class Program
         //縦列のチェック
         for (int j = 0; j < 3; j++)
         {
-            if (input[0, j] == target && input[1, j] == target && input[2, j] == target)
+            if (state[0, j] == target && state[1, j] == target && state[2, j] == target)
             {
                 return true;
             }
         }
 
         //対角線のチェック
-        if ((input[0, 0] == target && input[1, 1] == target && input[2, 2] == target)
-                || (input[0, 2] == target && input[1, 1] == target && input[2, 0] == target))
+        if ((state[0, 0] == target && state[1, 1] == target && state[2, 2] == target)
+                || (state[0, 2] == target && state[1, 1] == target && state[2, 0] == target))
         {
             return true;
         }
@@ -259,7 +264,7 @@ public class Program
         return false;
     }
 
-    private static void GameOver(string result)//「ゲーム終了です」と表示
+    private static void GameOver(int result)//「ゲーム終了です」と表示
     {
         Console.WriteLine("ゲーム終了、" + result + "の勝ちです");
     }
